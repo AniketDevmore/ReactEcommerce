94307b3eccd255b0315c5fb6119dc55ec9df18e9
Aniket Devmore <aniketdevmore1008@gmail.com>
2024-10-23 22:45:29 +0530

    Added basic login structure

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -1,61 +1,67 @@
 import React, { useState } from "react";
+import "./Login.css"
 import { Box, FilledInput, FormControl, IconButton, InputAdornment, InputLabel, TextField } from "@mui/material";
-import {Visibility} from '@mui/icons-material';
-import {VisibilityOff} from '@mui/icons-material';
+import { Visibility } from '@mui/icons-material';
+import { VisibilityOff } from '@mui/icons-material';
+import loginBackground from '../../../public/loginBackground2.jpg'
 
 const Login = () => {
     console.log('first')
     const [showPassword, setShowPassword] = useState(false);
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
-  
+
     const handleMouseDownPassword = (event) => {
-      event.preventDefault();
+        event.preventDefault();
     };
-  
+
     const handleMouseUpPassword = (event) => {
-      event.preventDefault();
+        event.preventDefault();
     };
 
     return (
-        <div>
-            <Box
-                component="form"
-                sx={{ '& > :not(style)': { m: 1, width: '35ch' } }}
-                noValidate
-                autoComplete="off"
-            >
-                <TextField id="filled-basic" label="User Name" variant="filled" />
-                {/* <TextField
+        <div className="loginContainer">
+            <img src={loginBackground} alt='farming' className="imageBackground" />
+            <div id="inputContainer">
+                <Box
+                    component="form"
+                    sx={{ '& > :not(style)': { m: 1, width: '70%' } }}
+                    noValidate
+                    autoComplete="off"
+                    className="box"
+                >
+                    <TextField id="filled-basic" label="User Name" variant="filled" value={'aaa'}/>
+                    {/* <TextField
                     required
                     id="filled-required"
                     label="User Name"
                     defaultValue="Hello World"
                     variant="filled"
                 /> */}
-                <FormControl sx={{ m: 1, width: '25ch' }} variant="filled">
-                    <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
-                    <FilledInput
-                        id="filled-adornment-password"
-                        type={showPassword ? 'text' : 'password'}
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={handleClickShowPassword}
-                                    onMouseDown={handleMouseDownPassword}
-                                    onMouseUp={handleMouseUpPassword}
-                                    edge="end"
-                                >
-                                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                    />
-                </FormControl>
+                    <FormControl sx={{ m: 1, width: '25ch' }} variant="filled">
+                        <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
+                        <FilledInput
+                            id="filled-adornment-password"
+                            value={'ppp'}
+                            type={showPassword ? 'text' : 'password'}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        onMouseUp={handleMouseUpPassword}
+                                        edge="end"
+                                    >
+                                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </FormControl>
 
-            </Box>
-
+                </Box>
+            </div>
         </div>
     )
 }
